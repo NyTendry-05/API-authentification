b239ef048383c4b51ddffe1b73d7144d2a64699e
Yoannah05 <166911727+Yoannah05@users.noreply.github.com>
2024-12-17 15:35:37 +0300

    création de la classe utilisateur

---
+++ src/main/java/mg/itu/api/services/UtilisateurService.java
@@ -0,0 +1,32 @@
+package mg.itu.api.services;
+
+import mg.itu.api.models.Utilisateur;
+import mg.itu.api.repositories.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UtilisateurService {
+
+    private final UtilisateurRepository utilisateurRepository;
+
+    @Autowired
+    public UtilisateurService(UtilisateurRepository utilisateurRepository) {
+        this.utilisateurRepository = utilisateurRepository;
+    }
+
+    public Utilisateur saveUtilisateur(Utilisateur utilisateur) {
+        return utilisateurRepository.save(utilisateur);
+    }
+
+    public Optional<Utilisateur> getUtilisateurById(Long id) {
+        return utilisateurRepository.findById(id);
+    }
+
+    public Optional<Utilisateur> getUtilisateurByEmail(String email) {
+        return utilisateurRepository.findByEmail(email);
+    }
+}
+
