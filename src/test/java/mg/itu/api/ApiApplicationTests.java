ff64680f6aa8c46215ddb0f664f39178bcf66300
NyTendry <nytendry.rakotondrazafy@gmail.com>
2024-12-17 11:46:29 +0300

    spring

---
+++ src/test/java/mg/itu/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.itu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
